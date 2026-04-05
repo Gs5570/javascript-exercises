15aa1944c03b6876df45ac061424bd4ee36b4c89
Galekwan Sango <galekwansango@gmail.com>
2026-04-05 11:30:43 -0400

    passed all the test case

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let numWatch = 0;
+  let newString = '';
+  let stdOut = 'ERROR';
 
+  if (num > 0) {
+    while (numWatch < num) {
+      newString += string;
+      numWatch++;
+    }
+    return newString;
+  } else if (num == 0) {
+    return newString;
+  } else {
+    if (string == '') {
+      return newString;
+    }
+    return stdOut;
+  }
 };
 
 // Do not edit below this line
