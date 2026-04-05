14173820aeb24ab3dcaf59ef09604aa5dd3873b0
Galekwan Sango <galekwansango@gmail.com>
2026-04-05 18:45:19 -0400

    passed all the test case. Implemented the rest paramteer to take multiple argument

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayParam, ...valueToBeRemoved) {
+  let watchArray = [];
+  for (let i = 0; i < arrayParam.length; i++) {
+    // use watch value to skip the index that has the element that is the value to the  be removed form the array;
+    let shouldRemove = false;
+    for (let j = 0; j < valueToBeRemoved.length; j++) {
+      if (valueToBeRemoved[j] === arrayParam[i]) {
+        shouldRemove = true;
+        break;
+      }
+    }
+    if (!shouldRemove) {
+      watchArray.push(arrayParam[i]);
+    }
+  }
+  return watchArray;
 };
 
 // Do not edit below this line
