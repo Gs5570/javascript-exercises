0a533c0eaba2cc49a3a53866fb1a7567722cebce
Galekwan Sango <galekwansango@gmail.com>
2026-04-05 17:02:56 -0400

    passed all the test cased, successfully implemeted string reversal

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (inputToBeReversed) {
+  let watchArray = [];
+  let newStrings = inputToBeReversed.split('');
+  console.log(newStrings);
 
+  for (let i = newStrings.length - 1; i >= 0; i--) {
+    watchArray.push(newStrings[i]);
+  }
+  return watchArray.join('');
 };
 
 // Do not edit below this line
