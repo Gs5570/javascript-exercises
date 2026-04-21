0c023bf7c4e6ba52a3620b2d781b8ec8a39f2673
Galekwan Sango <galekwansango@gmail.com>
2026-04-21 10:42:17 -0400

    passed all test cases

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (value) {
+  const cleaned = value.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let isPalindromes = false;
+  let valueArr = cleaned.split('');
+  let reverseArr = [];
+  let newValueArr = '';
+  let newReverseArr = '';
 
+  for (let i = valueArr.length - 1; i >= 0; i--) {
+    reverseArr.push(valueArr[i]);
+  }
+
+  newValueArr = valueArr.join('');
+  newReverseArr = reverseArr.join('');
+
+  console.log(newValueArr);
+  console.log(newReverseArr);
+
+  if (newValueArr.localeCompare(newReverseArr) == 0) {
+    isPalindromes = true;
+    return isPalindromes;
+  } else {
+    return isPalindromes;
+  }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
